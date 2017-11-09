b737fc749d71dd41f29704e99703a894794c3bdc
ann221d <beldiman.diana@gmx.de>
2017-11-09 13:46:01 +0100

    Step 170 - bis blocks

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,17 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  sides = [a,b,c].sort
+  raise TriangleError if sides.any? { |side| side <= 0 }
+  raise TriangleError unless (sides[0] + sides[1]) > sides[2]
+  sides.uniq!
+  if sides.count == 1
+    :equilateral
+  elsif sides.count == 2
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.

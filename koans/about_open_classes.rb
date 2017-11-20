e2311994ef2e02f4cb1e9bfdcbffc914ee65c093
ann221d <beldiman.diana@gmx.de>
2017-11-20 16:28:24 +0100

    Step 210

--- koans/about_open_classes.rb
+++ koans/about_open_classes.rb
@@ -9,7 +9,7 @@ class AboutOpenClasses < Neo::Koan
 
   def test_as_defined_dogs_do_bark
     fido = Dog.new
-    assert_equal __, fido.bark
+    assert_equal "WOOF", fido.bark
   end
 
   # ------------------------------------------------------------------
@@ -23,8 +23,8 @@ class AboutOpenClasses < Neo::Koan
 
   def test_after_reopening_dogs_can_both_wag_and_bark
     fido = Dog.new
-    assert_equal __, fido.wag
-    assert_equal __, fido.bark
+    assert_equal "HAPPY", fido.wag
+    assert_equal "WOOF", fido.bark
   end
 
   # ------------------------------------------------------------------

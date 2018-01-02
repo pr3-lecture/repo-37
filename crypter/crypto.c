f73b538cca8e10637197b801afc74d51b16e58ab
ann221d <beldiman.diana@gmx.de>
2018-01-02 18:17:52 +0100

    crypto.c

---
+++ crypter/crypto.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "crypto.h"
+#include<string.h>
+
+int keytest(KEY key){
+	if(key.type != 1) {
+        return KEY_ILLEGAL_TYPE;
+    }
+    
+    int keylength = strlen(key.chars);
+    if(keylength < 1) {
+        return KEY_TOO_SHORT;
+    }
+	
+    for(int i = 0; i < keylength; i++) {
+        char key_char = key.chars[i];
+        if(key_char < '@' || key_char > '_') {
+            return KEY_ILLEGAL_CHAR;
+        }
+    }
+	return 0;
+}
+
+int crypt(KEY key, const char *input, char* output, char lowlimit, char upperlimit, int errorName) {
+    int error = keytest(key);
+    if(error != 0) {
+        return error;
+    }
+
+    int keylength = strlen(key.chars);
+    int len = strlen(input);
+    int i = 0;
+    for(i = 0; len; i++) {
+        //validate current input char
+        char input_char = input[i];
+        if(input_char < lowlimit || input_char > upperlimit) {
+            return errorName;
+        }
+	}
+	
+	int i = 0;
+	int j = 0;
+	
+	
+	for (i, j; i < len; i++) {
+		output[i] = (((input[i] - 'A' + 1) ^ (key.chars[j] - 'A' + 1)) + 'A' - 1);
+		j = (j + 1) % keylength;
+	}
+	return 0;
+}
+	  
+
+int encrypt(KEY key, const char* input, char* output) {
+   return crypt(key,input,output,'A','Z', MESSAGE_ILLEGAL_CHAR);
+}
+
+int decrypt(KEY key, const char* input, char* output) {
+	return crypt(key,input,output,'@','_', CRYPTER_ILLEGAL_CHAR);
+}
+
+char* cryptoErrorString(int error) {
+    switch (error) {
+        case KEY_TOO_SHORT: return "Length of key not sufficient.";
+        case KEY_ILLEGAL_CHAR: return "Key contains illegal characters.";
+        case MESSAGE_ILLEGAL_CHAR: return "Message contains illegal characters.";
+        case CYPHER_ILLEGAL_CHAR: return "Cypher text contains illegal characters.";
+        case KEY_ILLEGAL_TYPE: return "Key type is illegal.";
+        default: return "No error";
+    }
+}

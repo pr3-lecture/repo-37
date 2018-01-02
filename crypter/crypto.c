f682c9387a64200f4b254e2945ef20f821cbde40
ann221d <beldiman.diana@gmx.de>
2018-01-02 21:22:45 +0100

    update

--- crypter/crypto.c
+++ crypter/crypto.c
@@ -4,27 +4,27 @@
 #include<string.h>
 
 int checkIllegalChars(const char* input, const char* allowedchars) {
-  
-  for (int i = 0; i < strlen(input); i++) {
-    int found = 0;
-    for (int j = 0; j < strlen(allowedchars); j++) {
-      if (input[i] == allowedchars[j]) {
-        found = 1;
-        break;
-      }
-    }
-    if (found == 0) 
-      return 1;
-  }
 
-  return 0;
+	for (int i = 0; i < strlen(input); i++) {
+		int found = 0;
+		for (int j = 0; j < strlen(allowedchars); j++) {
+			if (input[i] == allowedchars[j]) {
+				found = 1;
+				break;
+			}
+		}
+		if (found == 0)
+			return 1;
+	}
+
+	return 0;
 }
 
 int crypt(KEY key, const char *input, char* output) {
-   
-    int keylength = strlen(key.chars);
-    int len = strlen(input);
-    int i = 0;
+
+	int keylength = strlen(key.chars);
+	int len = strlen(input);
+	int i = 0;
 	int j = 0;
 	for (i, j; i < len; i++) {
 		output[i] = (((input[i] - 'A' + 1) ^ (key.chars[j] - 'A' + 1)) + 'A' - 1);
@@ -32,36 +32,36 @@ int crypt(KEY key, const char *input, char* output) {
 	}
 	return 0;
 }
-	  
+
 
 int encrypt(KEY key, const char* input, char* output) {
-	 if (strlen(key.chars) == 0)
-    return E_KEY_TOO_SHORT;
+	if (strlen(key.chars) == 0)
+		return E_KEY_TOO_SHORT;
 
-  if (checkIllegalChars(key.chars, KEY_CHARACTERS))
-    return E_KEY_ILLEGAL_CHAR;
+	if (checkIllegalChars(key.chars, KEY_CHARACTERS))
+		return E_KEY_ILLEGAL_CHAR;
 
-  if (checkIllegalChars(input, MESSAGE_CHARACTERS))
-    return E_MESSAGE_ILLEGAL_CHAR;
+	if (checkIllegalChars(input, MESSAGE_CHARACTERS))
+		return E_MESSAGE_ILLEGAL_CHAR;
 
-  crypt(key, input, output);
+	crypt(key, input, output);
 
-  return 0;
-   
+	return 0;
+
 }
 
 int decrypt(KEY key, const char* input, char* output) {
- if (strlen(key.chars) == 0)
-    return E_KEY_TOO_SHORT;
+	if (strlen(key.chars) == 0)
+		return E_KEY_TOO_SHORT;
 
- if (checkIllegalChars(key.chars, KEY_CHARACTERS))
-    return E_KEY_ILLEGAL_CHAR;
+	if (checkIllegalChars(key.chars, KEY_CHARACTERS))
+		return E_KEY_ILLEGAL_CHAR;
 
- if (checkIllegalChars(input, MESSAGE_CHARACTERS))
-    return E_MESSAGE_ILLEGAL_CHAR;
+	if (checkIllegalChars(input, MESSAGE_CHARACTERS))
+		return E_MESSAGE_ILLEGAL_CHAR;
 
-  crypt(key, input, output);
+	crypt(key, input, output);
 
-  return 0;
+	return 0;
 }
 
--- crypter/test.c
+++ crypter/test.c
@@ -6,110 +6,110 @@
 #define mu_assert(message, test) do { if (!(test)) return message; } while (0)
 #define mu_run_test(test) do { char *message = test(); tests_run++; \
                                 if (message) return message; } while (0)
-									
+
 static int tests_run = 0;
 
-							
+
 static char* test_key_too_short() {
-	KEY temporaryKey = {1, ""};
+	KEY temporaryKey = { 1, "" };
 	const char* input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char* output = (char*) malloc(sizeof(char) * (strlen(input) + 1));
-	
-    int result = encrypt(temporaryKey, input, output);
-    mu_assert("encrypt fail -> key too short", result == E_KEY_TOO_SHORT);
+	char* output = (char*)malloc(sizeof(char) * (strlen(input) + 1));
 
-    result = decrypt(temporaryKey, input, output);
-    mu_assert("decrypt fail -> key too short", result == E_KEY_TOO_SHORT);
+	int result = encrypt(temporaryKey, input, output);
+	mu_assert("encrypt fail -> key too short", result == E_KEY_TOO_SHORT);
 
-    return 0;
+	result = decrypt(temporaryKey, input, output);
+	mu_assert("decrypt fail -> key too short", result == E_KEY_TOO_SHORT);
+
+	return 0;
 }
 
 static char* test_key_contains_illegal_char() {
-	KEY temporaryKey = {1, "+"};
+	KEY temporaryKey = { 1, "+" };
 	const char* input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char* output = (char*) malloc(sizeof(char) * (strlen(input) + 1));
-	
-    int result = encrypt(temporaryKey, input, output);
-    mu_assert("encrypt fail -> key has illegal chars", result == E_KEY_ILLEGAL_CHAR);
+	char* output = (char*)malloc(sizeof(char) * (strlen(input) + 1));
 
-    result = decrypt(temporaryKey, input, output);
-    mu_assert("decrypt fail -> key has illegal chars", result == E_KEY_ILLEGAL_CHAR);
+	int result = encrypt(temporaryKey, input, output);
+	mu_assert("encrypt fail -> key has illegal chars", result == E_KEY_ILLEGAL_CHAR);
 
-    return 0;
-} 
+	result = decrypt(temporaryKey, input, output);
+	mu_assert("decrypt fail -> key has illegal chars", result == E_KEY_ILLEGAL_CHAR);
+
+	return 0;
+}
 
 static char* test_encrypt_input_contians_illegal_char() {
-	KEY temporaryKey = {1, "TPERULES"};
+	KEY temporaryKey = { 1, "TPERULES" };
 	const char* input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ+";
-	char* output = (char*) malloc(sizeof(char) * (strlen(input) + 1));
-	
-    int result = encrypt(temporaryKey, input, output);
-    mu_assert("encrypt fail -> input has illegal chars", result == E_MESSAGE_ILLEGAL_CHAR);
+	char* output = (char*)malloc(sizeof(char) * (strlen(input) + 1));
 
-    return 0;
+	int result = encrypt(temporaryKey, input, output);
+	mu_assert("encrypt fail -> input has illegal chars", result == E_MESSAGE_ILLEGAL_CHAR);
+
+	return 0;
 }
 
 static char* test_decrypt_input_contians_illegal_char() {
-	KEY temporaryKey = {1, "TPERULES"};
+	KEY temporaryKey = { 1, "TPERULES" };
 	const char* input = "URFVPJB[]ZN^XBJCEBVF@ZRKMJ+";
-	char* output = (char*) malloc(sizeof(char) * (strlen(input) + 1));
-	
-    int result = encrypt(temporaryKey, input, output);
-    mu_assert("decrypt fail -> input has illegal chars", result == E_MESSAGE_ILLEGAL_CHAR);
+	char* output = (char*)malloc(sizeof(char) * (strlen(input) + 1));
 
-    return 0;
+	int result = encrypt(temporaryKey, input, output);
+	mu_assert("decrypt fail -> input has illegal chars", result == E_MESSAGE_ILLEGAL_CHAR);
+
+	return 0;
 }
 
 static char* test_encrypt() {
 	char *key = "TPERULES";
-    KEY temporaryKey;
+	KEY temporaryKey;
 	temporaryKey.type = 1;
-    temporaryKey.chars = key;
+	temporaryKey.chars = key;
 
 	char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char *output = (char*)malloc(sizeof(char) * (strlen(input) + 1));
-    
-    encrypt(temporaryKey, input, output);
-    mu_assert("output error from encrypt", strcmp(output, "URFVPJB[]ZN^XBJCEBVF@ZRKMJ") == 0);
-	
-    free(output);
-    return 0;
+	char *output = (char*)malloc(sizeof(char) * (strlen(input) + 1));
+
+	encrypt(temporaryKey, input, output);
+	mu_assert("output error from encrypt", strcmp(output, "URFVPJB[]ZN^XBJCEBVF@ZRKMJ") == 0);
+
+	free(output);
+	return 0;
 }
 
 static char* test_decrypt() {
 	char *key = "TPERULES";
-    KEY temporaryKey;
+	KEY temporaryKey;
 	temporaryKey.type = 1;
-    temporaryKey.chars = key;
+	temporaryKey.chars = key;
 
 	char *input = "URFVPJB[]ZN^XBJCEBVF@ZRKMJ";
-    char *output = (char*)malloc(sizeof(char) * (strlen(input) + 1));
-    
-    encrypt(temporaryKey, input, output);
-    mu_assert("output error from decrypt", strcmp(output, "ABCDEFGHIJKLMNOPQRSTUVWXYZ") == 0);
-	
-    free(output);
-    return 0;
+	char *output = (char*)malloc(sizeof(char) * (strlen(input) + 1));
+
+	encrypt(temporaryKey, input, output);
+	mu_assert("output error from decrypt", strcmp(output, "ABCDEFGHIJKLMNOPQRSTUVWXYZ") == 0);
+
+	free(output);
+	return 0;
 }
 
 
 static char* allTests() {
-    mu_run_test(test_key_too_short);
+	mu_run_test(test_key_too_short);
 	mu_run_test(test_key_contains_illegal_char);
 	mu_run_test(test_encrypt_input_contians_illegal_char);
-    mu_run_test(test_decrypt_input_contians_illegal_char);
-    mu_run_test(test_encrypt);
+	mu_run_test(test_decrypt_input_contians_illegal_char);
+	mu_run_test(test_encrypt);
 	mu_run_test(test_decrypt);
-    return 0;
+	return 0;
 }
 
 int main() {
-    char *result = allTests();
+	char *result = allTests();
 
-    if (result != 0) printf("%s\n", result);
-    else             printf("ALL TESTS PASSED\n");
+	if (result != 0) printf("%s\n", result);
+	else             printf("ALL TESTS PASSED\n");
 
-    printf("Tests run: %d\n", tests_run);
+	printf("Tests run: %d\n", tests_run);
 
-    return result != 0;
+	return result != 0;
 }

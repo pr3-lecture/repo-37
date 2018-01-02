48cb76588e6376e26a3bae2ff16b8dae7579506b
ann221d <beldiman.diana@gmx.de>
2018-01-02 18:58:39 +0100

    test.c

--- crypter/test.c
+++ crypter/test.c
@@ -6,6 +6,7 @@
 #define mu_assert(message, test) do { if (!(test)) return message; } while (0)
 #define mu_run_test(test) do { char *message = test(); tests_run++; \
                                 if (message) return message; } while (0)
+									
 static int tests_run = 0;
 									
 static char* test_key_too_short() {
